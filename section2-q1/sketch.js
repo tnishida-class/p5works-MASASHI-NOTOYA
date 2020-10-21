043594d5cae9957e47185ac88337bc2d5cbcda2d
MASASHI-NOTOYA <1900602h@stu.kobe-u.ac.jp>
2020-10-22 01:11:29 +0900

    Update sketch.js

--- section2-q1/sketch.js
+++ section2-q1/sketch.js
@@ -1,6 +1,17 @@
 // 小手調べ
-function setup(){
+function setup()
+{
+  createCanvas(200,200);
+noFill();
   for(let i = 0; i < 10; i++){
     // BLANK[1]
-  }
+    if(i<5)
+    {
+      stroke(0,0,255);
+    }
+else {
+  stroke(255,0,0);
+}
+ellipse(width/2,height/2, width/(2*10)*(i+1));
+}
 }
