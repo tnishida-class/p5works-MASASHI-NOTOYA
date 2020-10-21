8ae9dc2bb55bd75f136e3edc59e6f0d7dbb2b814
MASASHI-NOTOYA <1900602h@stu.kobe-u.ac.jp>
2020-10-22 01:11:55 +0900

    Update sketch.js

--- section2-q2/sketch.js
+++ section2-q2/sketch.js
@@ -6,6 +6,49 @@ function setup() {
   for(let i = 0; i < 8; i++){
     for(let j = 0; j < 8; j++){
       // BLANK[1] (hint: rectのx座標は size * i, y座標はsize * j)
+      if(i%2==0)
+      {
+        if(j%2==0)
+            fill(255,255,255);
+        else
+            fill(128, 128, 128);
+      }
+      else
+      {
+        if(j%2!=0)
+          fill(255,255,255);
+        else
+          fill(128, 128, 128);
+      }
+        rect(size*i, size*j, size, size);
+
+
+
+
+        if(i%2==0)
+        {
+          if(j%2!=0)
+          {
+              fill(255, 0, 0);
+              if(j<=2)
+                ellipse(size*i+size/2, size*j+size/2, size-5);
+                fill(0, 0, 0);
+                if(j>=5)
+                  ellipse(size*i+size/2, size*j+size/2, size-5);
+          }
+        }
+        else
+        {
+          if(j%2==0)
+          {
+            fill(255, 0, 0);
+            if(j<=2)
+              ellipse(size*i+size/2, size*j+size/2, size-5);
+              fill(0, 0, 0);
+              if(j>=5)
+                ellipse(size*i+size/2, size*j+size/2, size-5);
+          }
+        }
     }
   }
 }
