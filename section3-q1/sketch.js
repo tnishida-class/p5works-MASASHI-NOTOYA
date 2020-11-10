b4dfe491959d4ff3790b7ef0608f9106e4ad9bb2
MASASHI-NOTOYA <1900602h@stu.kobe-u.ac.jp>
2020-11-10 10:56:30 +0900

    Update sketch.js

--- section3-q1/sketch.js
+++ section3-q1/sketch.js
@@ -4,15 +4,20 @@
 function setup(){
   createCanvas(400, 400);
   background(255);
-  balloon("I love keyakizaka46");
+  balloon("Keyakizaka46 bloomed as Sakurazaka46", 50, 20, 94, 185, 84);
 }
 
-function balloon(t){
+function balloon(t, x, y, r, g, b){
   let w = textWidth(t);
   let h = textAscent() + textDescent();
   let p = 2;
-  fill(0);
-  rect(0, 0, w + p * 2, h + p * 2);
+  noStroke();
+
+  fill(r, g, b);
+  rect(x, y, w + p * 2, h + p * 2);
+  triangle(x + 5, y + h+p*2, x + 10, y + 46, x + 20, y + h+p*2);
+
   fill(255);
-  text(t, p, h + p);
+  text(t, x + p, y + h + p);
+
 }
