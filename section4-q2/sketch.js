380aa438d3987d0975dc6a6d6afeaf538fab160e
MASASHI-NOTOYA <1900602h@stu.kobe-u.ac.jp>
2020-11-18 13:28:10 +0900

    4-q2

--- section4-q2/sketch.js
+++ section4-q2/sketch.js
@@ -1,18 +1,20 @@
 // テキスト「アニメーション」
 let x, y, vx, vy;
-
+let angle=0;
 function setup(){
   createCanvas(windowWidth, windowHeight);
   x = width / 2;
   y = height / 2;
-  vx = 8;
-  vy = 8;
+  vx = 4;
+  vy = 4;
 }
 
 function draw(){
   background(160, 192, 255);
-  // BLANK[2] (hint: 作った star 関数を使います)
-
+  star(x, y, 20, angle);// BLANK[2] (hint: 作った star 関数を使います)
+  angle++;
+  x += vx;
+  y += vy;
   // 端の処理パターン (1) 反対側から出てくる
   if(x > width){ x = 0; }
   else if(x < 0){ x = width; }
@@ -22,8 +24,8 @@ function draw(){
 
 function star(cx, cy, r, angle){
   beginShape();
-  for(var i = 0; i < 20; i++){
-    var theta = TWO_PI * i * 2 / 5 - HALF_PI;
+  for(var i = 0; i < 5; i++){
+    var theta = TWO_PI * i * 2 / 5 - HALF_PI + (angle%360)*(TWO_PI/360);
     // BLANK[1] (hint: angle 分だけ星を回転させるには？)
     var x = cx + cos(theta) * r;
     var y = cy + sin(theta) * r;
